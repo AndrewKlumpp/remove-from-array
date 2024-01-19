50e6af10910d07aebb8a8d59fcae77dfc46bd89c
AndrewKlumpp <AndrewOzcyKlumpp@gmail.com>
2024-01-19 13:52:33 -0700

    initial commit

---
+++ popper.js
@@ -0,0 +1,11 @@
+function popper(array, num) {
+
+}
+
+let arr1 = ['a', 'b', 'c', 'd', 'e'];
+console.log(popper(arr1, 2)); // [ 'e', 'd' ]
+console.log(arr1); // [ 'a', 'b', 'c' ]
+
+let arr2 = ['kale', 'spinach', 'collard greens', 'cabbage'];
+console.log(popper(arr2, 1)); // [ 'cabbage' ]
+console.log(arr2); // [ 'kale', 'spinach', 'collard greens' ]
