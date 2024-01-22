2be0fab2c86a9f91087feea19ef6d83b789d822b
AndrewKlumpp <AndrewOzcyKlumpp@gmail.com>
2024-01-22 09:18:33 -0700

    initial commit

---
+++ rotate.js
@@ -0,0 +1,12 @@
+let rotate = function(array, num) {
+
+};
+
+
+let arr = ['a', 'b', 'c', 'd', 'e'];
+rotate(arr, 2);
+console.log(arr); // [ 'd', 'e', 'a', 'b', 'c' ]
+
+let animals = ['wombat', 'koala', 'opossum', 'kangaroo'];
+rotate(animals, -1);
+console.log(animals); // [ 'koala', 'opossum', 'kangaroo', 'wombat' ]
